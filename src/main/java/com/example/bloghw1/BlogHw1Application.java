39809d0282d120504ed4c44f32fa88f185e158ec
Dsys1129 <dsys1129@gmail.com>
2023-06-23 13:19:36 +0900

    Init commit

---
+++ src/main/java/com/example/bloghw1/BlogHw1Application.java
@@ -0,0 +1,13 @@
+package com.example.bloghw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogHw1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogHw1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bloghw1/BlogHw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloghw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogHw1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
